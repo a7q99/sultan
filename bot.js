e9a19d5244a224e56627c376de89ec3aa02bc1b6
a7q99 <46059490+a7q99@users.noreply.github.com>
2018-12-22 03:11:09 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,7 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+    client.user.setGame( لا احد يهتم  ,'https://www.twitch.tv/A7Q9')
+
+});
+client.login(process.env.BOT_TOKEN);
