8fb359f90be742c8ac0b1ac90455f9445b3910e3
a7q99 <46059490+a7q99@users.noreply.github.com>
2018-12-23 18:27:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` لا احد يهتم  `,'https://www.twitch.tv/A7Q9')
+    client.user.setGame(` 💜  `,'https://www.twitch.tv/A7Q9')
 
 });
 client.login(process.env.BOT_TOKEN);
