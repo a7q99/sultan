c8c7a921263d25b20024dd92825ab2705b817e32
a7q99 <46059490+a7q99@users.noreply.github.com>
2018-12-25 19:54:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(` 💜  `,'https://www.twitch.tv/A7Q9')
+    client.user.setGame(` You Can get Everything by Force Except Respect and Love  `,'https://www.twitch.tv/A7Q9')
 
 });
 client.login(process.env.BOT_TOKEN);
