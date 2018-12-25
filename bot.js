136c750eeec82d6d178a2bd337147b7f61a45b7d
a7q99 <46059490+a7q99@users.noreply.github.com>
2018-12-25 19:56:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,4 @@ client.on('ready', () => {
     client.user.setGame(` You Can get Everything by Force Except Respect and Love  `,'https://www.twitch.tv/A7Q9')
 
 });
-client.login(process.env.BOT_TOKEN);
+client.login("MjcxNjc2Mzk3NDA2MTI2MDgy.DwPyUg.zqKP11fftcw8WFlPKlMOFL2kR54");
