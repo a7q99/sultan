0a6406b49ca6abfa640b4491d9abc97cc458acd1
a7q99 <46059490+a7q99@users.noreply.github.com>
2018-12-27 02:38:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  لا احد يهتم `,'https://www.twitch.tv/a7q9')
+    client.user.setGame(`  تستطيع الحصول على كل شيء بالقوة الا الاحترام والحب `,'https://www.twitch.tv/a7q9')
 
 });
 client.login(process.env.BOT_TOKEN);
