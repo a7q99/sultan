51d32cad6618cc562772fd3b16747f53b98cbf8a
a7q99 <46059490+a7q99@users.noreply.github.com>
2019-02-05 19:56:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  You can get everything by force except respect and love `,'https://www.twitch.tv/a7q9')
+    client.user.setGame(` سيبقى حذائي وكلام الناس أخوه للأبد  `,'https://www.twitch.tv/a7q9')
 
 });
 client.login(process.env.BOT_TOKEN);
