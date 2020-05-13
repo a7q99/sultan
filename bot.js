35cdf3ac242f5174bcfa25faa52190f88da596bc
a7q99 <46059490+a7q99@users.noreply.github.com>
2020-05-13 10:38:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,4 @@ client.on('ready', () => {
     client.user.setGame(` سيبقى حذائي وكلام الناس أخوه للأبد  `,'https://www.twitch.tv/a7q9')
 
 });
-client.login("NjgzNDEzNDYxMjYyMTM5NTgz.Xrujaw.90HFeFgJb60i3Ggt-kUU9Go5aE8");
+client.login();
